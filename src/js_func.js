1fceba90e3da22139160cb66d99be101dc933d4d
Deivid Almeida <mr.davialmeida@hotmail.com>
2023-09-15 18:30:40 -0300

    kljh

--- src/js_func.js
+++ src/js_func.js
@@ -1,15 +1,104 @@
-const deg2rad = (deg) => deg * (Math.PI / 180);
+const js_loop = (packings) => {
+  let cacheControlPoints = {}
+        let cacheCompanies = {}
 
+        const data = []
+        for (const [i, packing] of packings.entries()) {
+            // let object_temp = {}
+            // let current_control_point = null
 
-const js_func = (lat1, lon1, lat2, lon2) => {
-  const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1); // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance in km
+            // object_temp.current_control_point_name = '-'
+            // object_temp.current_control_point_type = '-'
 
-  return distance * 1000;
+            // if (packing.last_event_record) {
+            //     if (packing.last_event_record.type == 'inbound') {
+            //         if (
+            //             Object.keys(cacheControlPoints).includes(
+            //                 packing.last_event_record.control_point.toString()
+            //             )
+            //         ) {
+            //             current_control_point =
+            //                 cacheControlPoints[
+            //                     packing.last_event_record.control_point.toString()
+            //                 ]
+            //         } else {
+            //             // current_control_point = await ControlPoint.findById(
+            //             //     packing.last_event_record.control_point,
+            //             //     'name type'
+            //             // ).populate('type')
+
+            //             cacheControlPoints[
+            //                 packing.last_event_record.control_point.toString()
+            //             ] = current_control_point
+            //         }
+
+            //         if (current_control_point) {
+            //             object_temp.current_control_point_name =
+            //                 current_control_point.name
+            //             object_temp.current_control_point_type =
+            //                 current_control_point.type
+            //                     ? current_control_point.type.name
+            //                     : '-'
+            //         }
+            //     } else {
+            //         object_temp.current_control_point_name =
+            //             current_control_point
+            //                 ? current_control_point.name
+            //                 : '-'
+            //         object_temp.current_control_point_type =
+            //             current_control_point
+            //                 ? current_control_point.type.name
+            //                 : '-'
+            //     }
+            // }
+
+            // if (
+            //     Object.keys(cacheCompanies).includes(
+            //         packing.family.company.toString()
+            //     )
+            // ) {
+            //     company = cacheCompanies[packing.family.company.toString()]
+            // } else {
+            //     // company = await Company.findById(packing.family.company, 'name')
+            //     // cacheCompanies[packing.family.company.toString()] = company
+            //     company = null
+            // }
+
+            // object_temp._id = packing._id
+            // object_temp.tag = packing.tag.code
+            // object_temp.family_code = packing.family ? packing.family.code : '-'
+            // object_temp.serial = packing.serial
+            // object_temp.company = company ? company.name : '-'
+            // object_temp.current_state = packing.current_state
+
+            // //dados do último inbound/outbound
+            // object_temp.in_out_accuracy = packing.last_event_record
+            //     ? packing.last_event_record.accuracy
+            //     : '-'
+            // object_temp.in_out_date = packing.last_event_record
+            //     ? packing.last_event_record.created_at
+            //     : '-'
+
+            // //dados atuais
+            // object_temp.accuracy = packing.last_position
+            //     ? packing.last_position.accuracy
+            //     : '-'
+            // object_temp.date = packing.last_message_signal
+            //     ? packing.last_message_signal
+            //     : '-'
+
+            // object_temp.battery_percentage = packing.last_battery
+            //     ? packing.last_battery.battery
+            //     : '-'
+            // object_temp.battery_date = packing.last_battery
+            //     ? packing.last_battery.date
+            //     : '-'
+
+            data.push(packing)
+        }
+
+        return data
+
 }
 
-module.exports = js_func
+module.exports = js_loop
